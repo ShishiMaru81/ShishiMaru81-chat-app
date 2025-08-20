6170676efeb1c4e28ef946927f875eab8e78a7e2
Harshdeep Singh <harshdeepsingh1121@outlook.com>
2025-08-20 15:04:28 +0530

    Image domains add in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone',
+  images: {
+    domains: [
+      "lh3.googleusercontent.com",
+      "ik.imagekit.io"
+    ],
+  },
 };
 
 export default nextConfig;
