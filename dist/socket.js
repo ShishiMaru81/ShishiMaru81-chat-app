4951e9426e47747c830201e3ee718a5fca54ff51
Harshdeep Singh <harshdeepsingh1121@outlook.com>
2025-09-02 22:26:00 +0530

    Update CORS origin to use environment variable for flexibility

--- dist/socket.js
+++ dist/socket.js
@@ -8,7 +8,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000", // Next.js frontend
+        origin: process.env.ORIGIN || "http://localhost:3000", // Next.js frontend
         methods: ["GET", "POST"],
     },
 });
--- socket.ts
+++ socket.ts
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000", // Next.js frontend
+        origin: process.env.ORIGIN || "http://localhost:3000", // Next.js frontend
         methods: ["GET", "POST"],
     },
 });
