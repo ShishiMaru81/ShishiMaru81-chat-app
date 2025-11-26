245297e6d79a0fb5ffcf0b10069fd2333d98a8b0
Harshdeep Singh <harshdeepsingh1121@outlook.com>
2025-11-26 21:53:51 +0530

    feat: Add autoConnect option

--- src/lib/socketClient.tsx
+++ src/lib/socketClient.tsx
@@ -5,4 +5,5 @@ import { io, Socket } from "socket.io-client";
 export const socket: Socket = io(process.env.NEXT_PUBLIC_SOCKET_URL!, {
     transports: ["websocket"],
     withCredentials: true,
+    autoConnect: false,
 });
