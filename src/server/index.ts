d761db17aa9f788f03b0b060d86d69b657ae44b3
Harshdeep Singh <harshdeepsingh1121@outlook.com>
2026-02-20 19:34:48 +0530

    feat(server): add endpoint for internal message deletion and enhance CORS configuration

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,14 +3,34 @@ import express from "express";
 import http from "http";
 import cors from "cors";
 import { initSocket } from "./socket/index.js";
+import { emitToConversation } from "./socket/emit.js";
+import { socket } from "@/lib/socket/socketClient.jsx";
+import { SocketEvents } from "@/shared/types/SocketEvents.js";
 
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: process.env.ORIGIN,
+}));
+app.use(express.json());
 
 const server = http.createServer(app);
 
 await initSocket(server);
+
+
+app.post("/internal/message-deleted", (req, res) => {
+    console.log("🔌 internal/message-deleted", req.body);
+    const { conversationId, payload } = req.body;
+
+    if (!conversationId || !payload) {
+        return res.status(400).json({ error: "Invalid payload" });
+    }
+
+    emitToConversation(conversationId, SocketEvents.MESSAGE_DELETE, payload);
+
+    return res.json({ success: true });
+})
 
 server.listen(3001, () => {
     console.log("🚀 Server running on http://localhost:3001");
